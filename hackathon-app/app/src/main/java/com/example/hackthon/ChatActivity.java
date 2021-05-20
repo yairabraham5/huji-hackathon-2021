029077fa8237f3752524abb8be83f6867abaa692
talma <tal.mandelovitz@mail.huji.ac.il>
2021-05-20 12:15:34 +0300

    added chat activity

---
+++ hackathon-app/app/src/main/java/com/example/hackthon/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.example.hackthon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+}
