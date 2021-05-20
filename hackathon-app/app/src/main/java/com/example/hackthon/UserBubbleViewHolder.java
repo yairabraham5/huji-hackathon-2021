f93e59ba20c19133f7e7fb4370116b652a76e36c
Omer <omersalman@mail.huji.ac.il>
2021-05-20 15:40:07 +0300

    added user bubble view holder

--- hackathon-app/app/src/main/java/com/example/hackthon/UserBubbleViewHolder.java
+++ hackathon-app/app/src/main/java/com/example/hackthon/UserBubbleViewHolder.java
@@ -6,10 +6,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class BobbleViewHolder extends RecyclerView.ViewHolder {
+public class UserBubbleViewHolder extends RecyclerView.ViewHolder {
     TextView user_message;
 
-    public BobbleViewHolder(@NonNull  View itemView) {
+    public UserBubbleViewHolder(@NonNull  View itemView) {
         super(itemView);
         user_message = itemView.findViewById(R.id.user_message);
     }
